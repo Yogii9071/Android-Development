75fe2bf5c68090e26cc9b80f45fe2bd98739a1a9
Yogesh Kumar <yogesh.12222745@lpu.in>
2024-08-28 18:46:57 +0530

    Android dev

---
+++ ClassExample.kt
@@ -0,0 +1,10 @@
+class ClassExample //class name
+{  //class body start
+    fun intro(){ //function
+        println("Hi this side yogii")
+    }
+}//class body end
+fun main(){
+    val a = ClassExample() //instantiation
+    a.intro() //invocation(calling) of function from base class to main funtion
+}
